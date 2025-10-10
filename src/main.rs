a5bd272afd2d52c8b214c0fbca4d3d0cd1b22188
yeabsira <shimelisyeabsiragithub@gmail.com>
2025-10-10 10:36:30 -0700

    Initialize simple TCP server that listens on 127.0.0.1:4221 and logs new connections

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+
+    for stream in listener.incoming() {
+        match stream {
+            Result::Ok(stream) => {
+                println!("accepted new connection");
+            }
+            Result::Err(error) => {
+                println!("error: {}", error);
+            }
+        }
+    }
 }
